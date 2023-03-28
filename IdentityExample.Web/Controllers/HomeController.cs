7120972920c3e920be3686fb361c4b26d269bb65
Burak Ali ILDIR <burakaliildir@gmail.com>
2023-03-28 23:14:48 +0300

    build: register

--- IdentityExample.Web/Controllers/HomeController.cs
+++ IdentityExample.Web/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using IdentityExample.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using IdentityExample.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
 
 namespace IdentityExample.Web.Controllers
 {
@@ -8,9 +10,12 @@ namespace IdentityExample.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -25,6 +30,38 @@ namespace IdentityExample.Web.Controllers
 
         public IActionResult SignUp()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var identityResult = await _userManager.CreateAsync(new AppUser()
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                PhoneNumber = request.PhoneNumber,
+                City = string.Empty,
+            }, password: request.Password);
+
+
+            if (identityResult.Succeeded)
+            {
+                TempData["SuccessMessage"] = "Başarıyla kayıt oldunuz.";
+
+                return RedirectToAction(nameof(SignUp));
+            }
+
+            foreach (IdentityError error in identityResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
             return View();
         }
 
---
+++ IdentityExample.Web/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityExample.Web.ViewModels
+{
+    public class SignUpViewModel
+    {
+        public SignUpViewModel()
+        {
+        }
+
+        public SignUpViewModel(string userName, string email, string phoneNumber, string password)
+        {
+            UserName = userName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Password = password;
+        }
+
+        [Required(ErrorMessage = "Kullanıcı adı alanı boş geçilemez.")]
+        [DisplayName("Kullanıcı Adı :")]
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Email alanı boş geçilemez.")]
+        [EmailAddress(ErrorMessage = "Email formatı doğru değil.")]
+        [DisplayName("Email :")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Telefon numarası alanı boş geçilemez.")]
+        [DisplayName("Telefon :")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Parola alanı boş geçilemez.")]
+        [DisplayName("Parola :")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Parola tekrar alanı boş geçilemez.")]
+        [Compare(nameof(Password),ErrorMessage = "Parolalar uyuşmuyor.")]
+        [DisplayName("Parola Tekrar :")]
+        public string PasswordConfirm { get; set; }
+    }
+}
