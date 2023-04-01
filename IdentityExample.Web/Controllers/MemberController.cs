aac4233e4abd8b546a608473843ba1bfe2f5236a
Burak Ali ILDIR <burakaliildir@gmail.com>
2023-04-01 15:14:59 +0300

    dev: password change action

--- IdentityExample.Web/Controllers/MemberController.cs
+++ IdentityExample.Web/Controllers/MemberController.cs
@@ -36,10 +36,41 @@ namespace IdentityExample.Web.Controllers
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> PasswordChange(PasswordChangeViewModel request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("PasswordChange", "Member");
+            }
+
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            var checkPasswordResult = await _userManager.CheckPasswordAsync(user, request.OldPassword);
+
+            if (checkPasswordResult)
+            {
+                var response = await _userManager.ChangePasswordAsync(user, request.OldPassword, request.NewPassword);
+
+                if (response.Succeeded)
+                {
+                    await _userManager.UpdateSecurityStampAsync(user);
+                    await _signInManager.SignOutAsync();
+                    await _signInManager.PasswordSignInAsync(user, request.NewPassword, true, false);
+
+                    TempData["SuccessMessage"] = "Şifreniz başarıyla güncellendi";
+                    return RedirectToAction("PasswordChange", "Member");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Eski parolanız yanlış.");
+
+            return View();
+        }
+
         public async Task Logout()
         {
             await _signInManager.SignOutAsync();
         }
-
     }
 }
